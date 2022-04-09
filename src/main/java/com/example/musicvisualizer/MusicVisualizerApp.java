7541d15e9b36607797c7d4c49262fb139aa8d53c
ianta <ianta@IanTai.lan>
2022-04-09 15:29:43 -0600

    This is a refactored branch from the old file processing branch. Data is now printed simultaneously with the music, however it isn't integrated with the gui controller yet, so it only works for the one song hard coded in.

--- src/main/java/com/example/musicvisualizer/MusicVisualizerApp.java
+++ src/main/java/com/example/musicvisualizer/MusicVisualizerApp.java
@@ -6,34 +6,50 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
+import java.io.File;
 import java.io.IOException;
 
 public class MusicVisualizerApp extends Application {
-    @Override
-    public void start(Stage mainStage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MusicVisualizerApp.class.getResource("main-ui.fxml"));
-        Scene mainScene = new Scene(fxmlLoader.load());
-        mainStage.setTitle("Music Visualizer");
-        Image musicIcon = new Image(MusicVisualizerApp.class.getResource("/images/musicIcon.png").toString());
-        mainStage.getIcons().add(musicIcon);
-        mainScene.getStylesheets().add(getClass().getResource("/stylesheets/visualizerViolet.css").toExternalForm());
-        mainStage.setScene(mainScene);
-        mainStage.setResizable(false);
-        mainStage.show();
+	// all for the amplitudeProcessor class
+	private Media media;
+	private MediaPlayer mediaPlayer;
+	private File file = new File("src/main/resources/music/Perfumed Eves - Masayoshi Soken.mp3");
+	private String MEDIA_URL = file.toURI().toString();
 
-        mainStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-            @Override
-            public void handle(WindowEvent event) {
-                Platform.exit();
-                System.exit(0);
-            }
-        });
-    }
+	@Override
+	public void start(Stage mainStage) throws IOException {
+		// this works, now I've gotta figure out how to integrate it with the controller.
+		media = new Media(MEDIA_URL);
+		mediaPlayer = new MediaPlayer(media);
+		amplitudeProcessor ampProcessor = new amplitudeProcessor(mediaPlayer);
+		ampProcessor.addListener();
+		mediaPlayer.setAutoPlay(true);
 
-    public static void main(String[] args) {
-        launch();
-    }
+		FXMLLoader fxmlLoader = new FXMLLoader(MusicVisualizerApp.class.getResource("main-ui.fxml"));
+		Scene mainScene = new Scene(fxmlLoader.load());
+		mainStage.setTitle("Music Visualizer");
+		Image musicIcon = new Image(MusicVisualizerApp.class.getResource("/images/musicIcon.png").toString());
+		mainStage.getIcons().add(musicIcon);
+		mainScene.getStylesheets().add(getClass().getResource("/stylesheets/visualizerViolet.css").toExternalForm());
+		mainStage.setScene(mainScene);
+		mainStage.setResizable(false);
+		mainStage.show();
+
+		mainStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+			@Override
+			public void handle(WindowEvent event) {
+				Platform.exit();
+				System.exit(0);
+			}
+		});
+	}
+
+	public static void main(String[] args) {
+		launch();
+	}
 }
--- src/main/java/com/example/musicvisualizer/MusicVisualizerController.java
+++ src/main/java/com/example/musicvisualizer/MusicVisualizerController.java
@@ -4,6 +4,8 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.AreaChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
@@ -12,6 +14,7 @@ import javafx.scene.layout.Pane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.shape.SVGPath;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 
 import java.io.File;
@@ -22,203 +25,214 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 /**
- * Controller for all the music player's button functions.
- * Built with a lot of help from (<a href="https://www.youtube.com/watch?v=-D2OIekCKes">BroCode's JavaFX Music Player Tutorial</a>)
+ * Controller for all the music player's button functions. Built with a lot of
+ * help from (<a href="https://www.youtube.com/watch?v=-D2OIekCKes">BroCode's
+ * JavaFX Music Player Tutorial</a>)
+ * 
  * @author Rianna M, all methods unless otherwise stated.
  */
 public class MusicVisualizerController implements Initializable {
 
-    //Button Declaration
+	// Button Declaration
 
-    @FXML
-    private Pane pane;
+	@FXML
+	private Pane pane;
 
-    @FXML
-    private Label songLabel;
+	@FXML
+	private Label songLabel;
 
-    @FXML
-    private Button restartBtn, previousBtn, playPauseBtn, nextBtn, fileBtn;
+	@FXML
+	private Button restartBtn, previousBtn, playPauseBtn, nextBtn, fileBtn;
 
-    //Paths for SVG shapes for toggling play and pause button.
+	// Paths for SVG shapes for toggling play and pause button.
 
-    @FXML
-    SVGPath playPauseSVG = new SVGPath();
-    String playPath = "M361 215C375.3 223.8 384 239.3 384 256C384 272.7 375.3 288.2 361 296.1L73.03 472.1C58.21 482 39.66 482.4 24.52 473.9C9.377 465.4 0 449.4 0 432V80C0 62.64 9.377 46.63 24.52 38.13C39.66 29.64 58.21 29.99 73.03 39.04L361 215z";
-    String pausePath = "M272 63.1l-32 0c-26.51 0-48 21.49-48 47.1v288c0 26.51 21.49 48 48 48L272 448c26.51 0 48-21.49 48-48v-288C320 85.49 298.5 63.1 272 63.1zM80 63.1l-32 0c-26.51 0-48 21.49-48 48v288C0 426.5 21.49 448 48 448l32 0c26.51 0 48-21.49 48-48v-288C128 85.49 106.5 63.1 80 63.1z";
+	@FXML
+	SVGPath playPauseSVG = new SVGPath();
+	String playPath = "M361 215C375.3 223.8 384 239.3 384 256C384 272.7 375.3 288.2 361 296.1L73.03 472.1C58.21 482 39.66 482.4 24.52 473.9C9.377 465.4 0 449.4 0 432V80C0 62.64 9.377 46.63 24.52 38.13C39.66 29.64 58.21 29.99 73.03 39.04L361 215z";
+	String pausePath = "M272 63.1l-32 0c-26.51 0-48 21.49-48 47.1v288c0 26.51 21.49 48 48 48L272 448c26.51 0 48-21.49 48-48v-288C320 85.49 298.5 63.1 272 63.1zM80 63.1l-32 0c-26.51 0-48 21.49-48 48v288C0 426.5 21.49 448 48 448l32 0c26.51 0 48-21.49 48-48v-288C128 85.49 106.5 63.1 80 63.1z";
 
-    @FXML
-    private Slider volumeSlider;
+	@FXML
+	private Slider volumeSlider;
 
-    @FXML
-    private ProgressBar songProgressBar;
+	@FXML
+	private ProgressBar songProgressBar;
 
-    //Filepath Variables
+	// Filepath Variables
 
-    private File songDirectory;
+	private File songDirectory;
 
-    private File[] songFiles;
+	private File[] songFiles;
 
-    private ArrayList<File> songList;
+	private ArrayList<File> songList;
 
-    private int songNumber;
+	private int songNumber;
 
-    //Timers
+	// Timers
 
-    private Timer progBarTimer;
+	private Timer progBarTimer;
 
-    private TimerTask progBarTask;
+	private TimerTask progBarTask;
 
-    //Logic check for status of song.
+	// Logic check for status of song.
 
-    private boolean isPlaying; //if music is playing AT ALL (vs. being stopped)
-    private boolean isPaused = true; //if music is in a paused state. used to toggle the play/pause button
+	private boolean isPlaying; // if music is playing AT ALL (vs. being stopped)
+	private boolean isPaused = true; // if music is in a paused state. used to toggle the play/pause button
 
-    //MediaPlayer
+	// MediaPlayer
 
-    private Media songMedia;
-    private MediaPlayer songMediaPlayer;
+	private Media songMedia;
+	private MediaPlayer songMediaPlayer;
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        songList = new ArrayList<File>();
-        songDirectory = new File("src/main/resources/music");
-        songFiles = songDirectory.listFiles();
-        if(songFiles != null) {
-            for (File oneFile : songFiles) {
-                songList.add(oneFile);
-            }
-        }
+	@Override
+	public void initialize(URL url, ResourceBundle resourceBundle) {
+		songList = new ArrayList<File>();
+		songDirectory = new File("src/main/resources/music");
+		songFiles = songDirectory.listFiles();
+		if (songFiles != null) {
+			for (File oneFile : songFiles) {
+				songList.add(oneFile);
+			}
+		}
 
-        else{
-                System.err.print("ERROR: Song directory is incorrect or empty!");
-            }
+		else {
+			System.err.print("ERROR: Song directory is incorrect or empty!");
+		}
 
-        songMedia = new Media(songList.get(songNumber).toURI().toString());
-        songMediaPlayer = new MediaPlayer(songMedia);
-        songLabel.setText(songList.get(songNumber).getName().replaceFirst("[.][^.]+$", "")); //Filename is included, extension is truncated.
+		songMedia = new Media(songList.get(songNumber).toURI().toString());
 
-        volumeSlider.valueProperty().addListener(new ChangeListener<Number>() {
-            @Override
-            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-                songMediaPlayer.setVolume(volumeSlider.getValue() * 0.01);
-            }
-        });
-    }
+		songMediaPlayer = new MediaPlayer(songMedia);
+		songLabel.setText(songList.get(songNumber).getName().replaceFirst("[.][^.]+$", "")); // Filename is included,
+																								// extension is
+																								// truncated.
 
+		volumeSlider.valueProperty().addListener(new ChangeListener<Number>() {
+			@Override
+			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+				songMediaPlayer.setVolume(volumeSlider.getValue() * 0.01);
+			}
+		});
 
-    /**
-     * Plays and pauses the music.
-     */
-    public void playPauseMedia(){
-        if(isPaused == true){
-        beginTimer();
-        songMediaPlayer.play();
-        songMediaPlayer.setVolume(volumeSlider.getValue() * 0.01);
-        isPaused= false;
-        playPauseSVG.setContent(pausePath);
-        }
+	}
 
-        else{
-            isPaused = true;
-            playPauseSVG.setContent(playPath);
-            cancelTimer();
-            songMediaPlayer.pause();
-        }
-    }
+	/**
+	 * Plays and pauses the music.
+	 */
+	public void playPauseMedia() {
+		if (isPaused == true) {
+			beginTimer();
+			songMediaPlayer.play();
+			songMediaPlayer.setVolume(volumeSlider.getValue() * 0.01);
+			isPaused = false;
+			playPauseSVG.setContent(pausePath);
+		}
 
+		else {
+			isPaused = true;
+			playPauseSVG.setContent(playPath);
+			cancelTimer();
+			songMediaPlayer.pause();
+		}
+	}
 
-    /**
-     *  Restarts the music.
-     */
-    public void restartMedia(){
-        songProgressBar.setProgress(0);
-        songMediaPlayer.seek(Duration.seconds(0));
+	/**
+	 * Restarts the music.
+	 */
+	public void restartMedia() {
+		songProgressBar.setProgress(0);
+		songMediaPlayer.seek(Duration.seconds(0));
 
-        if(!isPlaying){
-            beginTimer();
-        }
-    }
+		if (!isPlaying) {
+			beginTimer();
+		}
+	}
 
-    /**
-     *  Stops the current song and then plays the previous song on the playlist.
-     */
-    public void previousMedia(){
-        if(songNumber > 0){
-            songNumber -=1;
-            songMediaPlayer.stop();
-        }
+	/**
+	 * Stops the current song and then plays the previous song on the playlist.
+	 */
+	public void previousMedia() {
+		if (songNumber > 0) {
+			songNumber -= 1;
+			songMediaPlayer.stop();
+		}
 
-        else{songNumber = songList.size() -1;} //returns to start of the playlist if the end of the playlist is reached.
+		else {
+			songNumber = songList.size() - 1;
+		} // returns to start of the playlist if the end of the playlist is reached.
 
-        if(isPlaying){
-            cancelTimer();
-        }
+		if (isPlaying) {
+			cancelTimer();
+		}
 
-        songMedia = new Media(songList.get(songNumber).toURI().toString());
-        songMediaPlayer = new MediaPlayer(songMedia);
-        songLabel.setText(songList.get(songNumber).getName().replaceFirst("[.][^.]+$", "")); //Filename is included, extension is truncated.
-        isPaused = true;
-        playPauseMedia(); //autoplays next song
-    }
+		songMedia = new Media(songList.get(songNumber).toURI().toString());
+		songMediaPlayer = new MediaPlayer(songMedia);
+		songLabel.setText(songList.get(songNumber).getName().replaceFirst("[.][^.]+$", "")); // Filename is included,
+																								// extension is
+																								// truncated.
+		isPaused = true;
+		playPauseMedia(); // autoplays next song
+	}
 
+	/**
+	 * Stops the current song and then plays the next song on the playlist.
+	 */
+	public void nextMedia() {
 
-    /**
-     *  Stops the current song and then plays the next song on the playlist.
-     */
-    public void nextMedia(){
+		if (songNumber < songList.size() - 1) {
+			songNumber += 1;
+			songMediaPlayer.stop();
+		}
 
-        if(songNumber < songList.size() -1){
-            songNumber +=1;
-            songMediaPlayer.stop();
-        }
+		else {
+			songNumber = 0;
+		} // returns to start of the playlist if the end of the playlist is reached.
 
-        else{songNumber = 0;} //returns to start of the playlist if the end of the playlist is reached.
+		if (isPlaying) {
+			cancelTimer();
+		}
 
-        if(isPlaying){
-            cancelTimer();
-        }
+		songMedia = new Media(songList.get(songNumber).toURI().toString());
+		songMediaPlayer = new MediaPlayer(songMedia);
+		songLabel.setText(songList.get(songNumber).getName().replaceFirst("[.][^.]+$", "")); // Filename is included,
+																								// extension is
+																								// truncated.
 
-        songMedia = new Media(songList.get(songNumber).toURI().toString());
-        songMediaPlayer = new MediaPlayer(songMedia);
-        songLabel.setText(songList.get(songNumber).getName().replaceFirst("[.][^.]+$", "")); //Filename is included, extension is truncated.
+		isPaused = true;
+		playPauseMedia(); // autoplays next song
+	}
 
-        isPaused = true;
-        playPauseMedia(); //autoplays next song
-    }
+	public void fileChooseMedia() {
+		// TODO WANTED ADDITIONAL FUNCTION
+	}
 
+	/**
+	 * Starts timer for progression bar visuals.
+	 */
+	public void beginTimer() {
+		progBarTimer = new Timer();
 
-    public void fileChooseMedia(){
-        //TODO WANTED ADDITIONAL FUNCTION
-    }
+		progBarTask = new TimerTask() {
+			public void run() {
+				isPlaying = true;
+				double currentTime = songMediaPlayer.getCurrentTime().toSeconds();
+				double endTime = songMedia.getDuration().toSeconds();
+				songProgressBar.setProgress(currentTime / endTime);
+				// System.out.println((currentTime / endTime) * 100); testing bar progression
+				// values.
+				if (currentTime / endTime == 1) {
+					cancelTimer();
+					isPaused = true; // test for debug
+				}
+			}
+		};
 
-    /**
-     * Starts timer for progression bar visuals.
-     */
-    public void beginTimer() {
-        progBarTimer = new Timer();
+		progBarTimer.scheduleAtFixedRate(progBarTask, 0, 1000);
+	}
 
-        progBarTask = new TimerTask() {
-            public void run() {
-                isPlaying = true;
-                double currentTime = songMediaPlayer.getCurrentTime().toSeconds();
-                double endTime = songMedia.getDuration().toSeconds();
-                songProgressBar.setProgress(currentTime / endTime);
-                //System.out.println((currentTime / endTime) * 100); testing bar progression values.
-                if (currentTime / endTime == 1) {
-                    cancelTimer();
-                    isPaused = true; //test for debug
-                }
-            }
-        };
-
-        progBarTimer.scheduleAtFixedRate(progBarTask, 0, 1000);
-    }
-
-    /**
-     * Cancels timer for progression bar visuals.
-     */
-    public void cancelTimer(){
-        isPlaying = false;
-        progBarTimer.cancel();
-    }
+	/**
+	 * Cancels timer for progression bar visuals.
+	 */
+	public void cancelTimer() {
+		isPlaying = false;
+		progBarTimer.cancel();
+	}
 }
